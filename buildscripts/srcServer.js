5a1ac91bdb26e136a5a7321a8ef1a49c4f559059
sivaramankj1 <sivaramankj1@gmail.com>
2017-11-12 19:53:41 +0530

    first commit

---
+++ buildscripts/srcServer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+var port = 3000;
+var app = express();
+app.get('/',function(req,res){
+    res.sendfile(path.join(__dirname,'../src/index.html'));
+});
+app.get('/test',function(req,res){
+    res.download(path.join(__dirname,'../src/sample.json'));
+});
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(err)
+    }
+    else{
+        //open("http://localhost:" + port);
+    }
+});
